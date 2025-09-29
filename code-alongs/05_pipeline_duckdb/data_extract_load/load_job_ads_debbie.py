0313100403009829682f8ab900a8fb9b069a8998
Marcus <marcus.ericsson_92@icloud.com>
2025-09-29 15:31:43 +0200

    orchestration (dlt asset, dbt asset, job, schedule, sensor and definition. code alongs with debbie

---
+++ code-alongs/05_pipeline_duckdb/data_extract_load/load_job_ads_debbie.py
@@ -0,0 +1,50 @@
+
+import dlt
+import requests
+import json
+from pathlib import Path
+import os
+
+params = {"limit": 100, "occupation-field": "6Hq3_tKo_V57"}
+
+#data warehouse directory
+# db_path = str(Path(__file__).parents[1] / "data_warehouse/job_ads.duckdb")
+
+def _get_ads(url_for_search, params):
+    headers = {"accept": "application/json"}
+    response = requests.get(url_for_search, headers=headers, params=params)
+    response.raise_for_status()  
+    return json.loads(response.content.decode("utf8"))
+
+
+@dlt.resource(table_name= "technical_field_job_ads", write_disposition="replace")
+def jobads_resource(params):
+
+    url = "https://jobsearch.api.jobtechdev.se"
+    url_for_search = f"{url}/search"
+
+    for ad in _get_ads(url_for_search, params)["hits"]:
+        yield ad
+
+
+# @dlt.source
+# def jobads_source():
+#     return jobads_resource(params)
+
+# def run_pipeline(table_name):
+#     pipeline = dlt.pipeline(
+#         pipeline_name="jobsearch",
+#         destination=dlt.destinations.duckdb(db_path), #update destination
+#         dataset_name="staging",
+#     )
+
+
+#     load_info = pipeline.run(jobads_resource(params=params), table_name=table_name)
+#     print(load_info)
+
+
+# if __name__ == "__main__":
+#     working_directory = Path(__file__).parent
+#     os.chdir(working_directory)
+
+#     run_pipeline(table_name="technical_field_job_ads")
---
+++ code-alongs/05_pipeline_duckdb/orchestration/definitions.py
@@ -0,0 +1,88 @@
+# session imports
+import dlt
+import dagster as dg
+from dagster_dlt import DagsterDltResource, dlt_assets
+from dagster_dbt import DbtCliResource, dbt_assets, DbtProject
+from pathlib import Path
+
+# to import dlt script
+import sys
+
+sys.path.insert(0, "../data_extract_load")
+from load_job_ads_debbie import jobads_source
+
+# data warehouse dircetory
+db_path = str(Path(__file__).parents[1] / "data_warehouse/job_ads.duckdb")
+# ---------------------------------------------------------------------------
+
+# dlt Assets
+## create dlt resource
+dlt_resource = DagsterDltResource()
+
+
+## create dlt asset
+@dlt_assets(
+    dlt_source=jobads_source(),
+    dlt_pipeline=dlt.pipeline(
+        pipeline_name="jobsearch",
+        dataset_name="staging",
+        destination=dlt.destinations.duckdb(db_path),
+    ),
+)
+def dlt_load(
+    context: dg.AssetExecutionContext, dlt: DagsterDltResource
+):  # <- if you need loggin data sparas
+    ### pydantic, dependency injection, signature: olika namn till dagster
+    yield from dlt.run(context=context)
+
+
+# ---------------------------------------------------------------------------------
+# dbt Assets
+## related paths for dbt project
+dbt_project_directory = Path(__file__).parents[1] / "data_transformation"
+profiles_dir = Path.home() / ".dbt"
+
+## create dagster dbt project object
+dbt_project = DbtProject(project_dir=dbt_project_directory, profiles_dir=profiles_dir)
+
+dbt_resource = DbtCliResource(project_dir=dbt_project)
+
+## create a manifest json file
+dbt_project.prepare_if_dev()
+
+
+## create dagster dbt asset
+@dbt_assets(manifest=dbt_project.manifest_path)
+def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
+    yield from dbt.cli(["dbt_build"], context=context).stream()
+
+#-------------------------------------------------------------
+
+# job
+job_dlt = dg.define_asset_job("job_dlt", selection = dg.AssetSelection.keys("dlt_jobads_source_jobads_resource"))
+
+
+job_dbt = dg.define_asset_job("job_dbt", selection= dg.AssetSelection.key_prefixes("warehouse", "mart"))
+
+# ------------------------------------------------------------------------------------------------------
+
+# Schedule
+schedule_dlt = dg.ScheduleDefinition(
+    job= job_dlt,
+    cron_schedule= "05 13 * * *"
+)
+# ---------------------------------------------------------
+
+# Sensor
+@dg.asset_sensor(asset_key= dg.AssetKey("dlt_jobads_source_jobads_resource"), job_name= "job_dbt")
+def dlt_load_sensor():
+    yield dg.RunRequest()
+
+# Definitions
+defs = dg.Definitions(
+    assets=[dlt_load, dbt_models],
+    resources={"dlt": dlt_resource, "dbt": dbt_resource},
+    jobs=[job_dlt, job_dbt],
+    schedules=[schedule_dlt],
+    sensors= [dlt_load_sensor]
+)
